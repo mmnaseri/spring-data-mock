d048ae72530a7a4952268502bf14277c83a1d1c7
mmnaseri <mmnaseri@programmer.net>
2015-09-23 13:05:04 -0700

    feat (domain): add all supported operators

--- src/main/java/com/mmnaseri/utils/spring/data/domain/Operator.java
+++ src/main/java/com/mmnaseri/utils/spring/data/domain/Operator.java
@@ -9,8 +9,30 @@ import com.mmnaseri.utils.spring.data.domain.impl.matchers.IsNotMatcher;
  */
 public enum Operator {
 
-    EQUAL_TO(1, new EqualToMatcher(), "Is", "EqualTo", "IsEqualTo", "Equals"),
-    IS_NOT(1, new IsNotMatcher(), "IsNot", "Not", "IsNotEqualTo", "DoesNotEqual"),
+    AFTER(1, null, "After", "IsAfter"),
+    BEFORE(1, null, "Before", "IsBefore"),
+    CONTAINING(1, null, "Containing", "IsContaining", "Contains"),
+    BETWEEN(2, null, "Between", "IsBetween"),
+    ENDING_WITH(1, null, "EndingWith", "IsEndingWith", "EndsWith"),
+    EXISTS(1, null, "Exists"),
+    FALSE(0, null, "False", "IsFalse"),
+    GREATER_THAN(1, null, "GreaterThan", "IsGreaterThan"),
+    GREATER_THAN_EQUALS(1, null, "GreaterThanEqual", "IsGreaterThanEqual"),
+    IN(1, null, "In", "IsIn"),
+    IS(1, new EqualToMatcher(), "Is", "EqualTo", "IsEqualTo", "Equals"),
+    IS_NOT_NULL(0, null, "NotNull", "IsNotNull"),
+    IS_NULL(0, null, "Null", "IsNull"),
+    LESS_THAN(1, null, "LessThan", "IsLessThan"),
+    LESS_THAN_EQUAL(1, null, "LessThanEqual", "IsLessThanEqual"),
+    LIKE(1, null, "Like", "IsLike"),
+    NEAR(1, null, "Near", "IsNear"),
+    NOT(1, new IsNotMatcher(), "IsNot", "Not", "IsNotEqualTo", "DoesNotEqual"),
+    NOT_IN(1, null, "NotIn", "IsNotIn"),
+    NOT_LIKE(1, null, "NotLike", "IsNotLike"),
+    REGEX(1, null, "Regex", "MatchesRegex", "Matches"),
+    STARTING_WITH(1, null, "StartingWith", "IsStartingWith", "StartsWith"),
+    TRUE(0, null, "True", "IsTrue"),
+    WITHIN(1, null, "Within", "IsWithin"),
     ;
     private final int operands;
     private final Matcher matcher;
