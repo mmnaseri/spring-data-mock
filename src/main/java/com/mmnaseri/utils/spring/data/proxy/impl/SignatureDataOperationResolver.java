92a04f3eba6b75ebd00fa662efa8535318b56a33
mmnaseri <mmnaseri@programmer.net>
2015-10-06 15:32:01 -0700

    feat (proxy): add support for a more generic method signature lookup

--- src/main/java/com/mmnaseri/utils/spring/data/proxy/impl/SignatureDataOperationResolver.java
+++ src/main/java/com/mmnaseri/utils/spring/data/proxy/impl/SignatureDataOperationResolver.java
@@ -4,7 +4,6 @@ import com.mmnaseri.utils.spring.data.domain.impl.MethodInvocationDataStoreOpera
 import com.mmnaseri.utils.spring.data.proxy.DataOperationResolver;
 import com.mmnaseri.utils.spring.data.proxy.TypeMapping;
 import com.mmnaseri.utils.spring.data.store.DataStoreOperation;
-import org.springframework.util.ReflectionUtils;
 
 import java.io.Serializable;
 import java.lang.reflect.Method;
@@ -26,11 +25,39 @@ public class SignatureDataOperationResolver implements DataOperationResolver {
     public DataStoreOperation<?, ?, ?> resolve(Method method) {
         for (TypeMapping<?> mapping : mappings) {
             final Class<?> type = mapping.getType();
-            final Method declaration = ReflectionUtils.findMethod(type, method.getName(), method.getParameterTypes());
+            final Method declaration = findMethod(type, method.getName(), method.getParameterTypes());
             if (declaration != null) {
                 final Object instance = mapping.getInstance();
                 return new MethodInvocationDataStoreOperation<Serializable, Object>(instance, declaration);
             }
+        }
+        return null;
+    }
+
+    private static Method findMethod(Class<?> type, String name, Class<?>... parameterTypes) {
+        Class<?> searchType = type;
+        while (searchType != null) {
+            final Method[] methods = searchType.isInterface() ? searchType.getMethods() : searchType.getDeclaredMethods();
+            for (Method method : methods) {
+                if (method.getName().equals(name)) {
+                    if (parameterTypes == null) {
+                        return method;
+                    } else if (parameterTypes.length == method.getParameterTypes().length) {
+                        boolean matches = true;
+                        for (int i = 0; i < parameterTypes.length; i++) {
+                            final Class<?> parameterType = parameterTypes[i];
+                            if (!method.getParameterTypes()[i].isAssignableFrom(parameterType)) {
+                                matches = false;
+                                break;
+                            }
+                        }
+                        if (matches) {
+                            return method;
+                        }
+                    }
+                }
+            }
+            searchType = searchType.getSuperclass();
         }
         return null;
     }
