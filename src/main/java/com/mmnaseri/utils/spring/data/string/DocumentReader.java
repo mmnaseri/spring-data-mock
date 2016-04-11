edc692523f542a7738e62accdd04a5604346cd03
Milad Naseri <milad.naseri@cdk.com>
2016-04-11 14:26:29 -0700

    feat(~.string.*): remove unused classes and methods

--- src/main/java/com/mmnaseri/utils/spring/data/string/DocumentReader.java
+++ src/main/java/com/mmnaseri/utils/spring/data/string/DocumentReader.java
@@ -1,8 +1,5 @@
 package com.mmnaseri.utils.spring.data.string;
 
-import com.mmnaseri.utils.spring.data.string.impl.ReaderSnapshot;
-
-import java.util.Set;
 import java.util.regex.Pattern;
 
 /**
@@ -10,18 +7,6 @@ import java.util.regex.Pattern;
  * @since 1.0 (9/19/15)
  */
 public interface DocumentReader {
-
-    /**
-     * This method will add a token reader to the document reader so that tokens can be read more easily
-     * @param reader    the reader to be registered with the document reader
-     */
-    void addReader(TokenReader reader);
-
-    /**
-     * This should give report as to where the reader stands within the document at this moment
-     * @return the part of document currently read through
-     */
-    String taken();
 
     /**
      * This will return the parts of document not yet read through by the reader
@@ -51,63 +36,10 @@ public interface DocumentReader {
     boolean has(String pattern);
 
     /**
-     * This method will determine if another token can be found in the document
-     * @return <code>true</code> if a valid token exists to be read
-     */
-    boolean hasTokens();
-
-    /**
-     * This method will determine if another token can be found in the document
-     * @param tokenReader    the token designator for discovering tokens
-     * @return <code>true</code> if a token that can be read by the given reader exists in the document
-     */
-    boolean hasTokens(TokenReader tokenReader);
-
-    /**
      * Determines whether we have hit the end of the document or not
      * @return <code>true</code> if we have no more to go
      */
     boolean hasMore();
-
-    /**
-     * Will return the next character
-     * @return next character in the document stream
-     */
-    char nextChar();
-
-    /**
-     * Will determine possible tokens that can be read from the document
-     * @return a set of all tokens that could be read from the document
-     */
-    Set<Token> nextToken();
-
-    /**
-     * Will give the next token. If no tokens can be found, the method will either take further action
-     * to fix this, or throw an Exception. You can confer {@link #hasTokens} to see if another token exists
-     * in the document.<br>
-     * <strong>NB</strong> This method will attempt to read the <em>first</em> identifiable token, meaning
-     * that if one token is the prefix of another, then the first will be discovered.
-     *
-     * @param tokenReader       the token designator
-     * @return next token (if any can be found)
-     */
-    Token nextToken(TokenReader tokenReader);
-
-    /**
-     * Works the same as {@link #nextToken(TokenReader)} with the exception that if the token cannot be read,
-     * an exception will be thrown
-     *
-     * @param tokenReader       the token designator
-     * @return next token (if any can be found)
-     */
-    Token expectToken(TokenReader tokenReader);
-
-    /**
-     * Takes in a token definition and reads the token from the cursor position.
-     * @param token    the token definition
-     * @return the token described by the parameter. {@code null} if no such token exists
-     */
-    String read(Token token);
 
     /**
      * Will attempt to read the string matching the given parameter. If the string matched with this pattern
@@ -169,14 +101,6 @@ public interface DocumentReader {
      * @param pattern            the regular to query for
      * @return the read string
      */
-    String expect(Pattern pattern);
-
-    /**
-     * This will attempt to read string matching the given pattern from the document at the current point
-     * indicated by the cursor. If failed to do so, the method will be expected to throw an exception or take corrective measures.
-     * @param pattern            the regular to query for
-     * @return the read string
-     */
     String expect(String pattern);
 
     /**
@@ -194,44 +118,6 @@ public interface DocumentReader {
      * and the line/column positioning data will be reset to their initial value.
      */
     void reset();
-
-    /**
-     * This method will give the control momentarily to the given {@link SnippetParser} instance.
-     * @param parser    the parser to give over the flow to
-     * @param <E>       the type of the snippet
-     * @return the data returned by the parser
-     */
-    <E> E parse(SnippetParser<E> parser);
-
-    /**
-     * Peeks ahead for the specified length of characters
-     * @param length    the number of characters to read
-     * @return the string with the specified length
-     */
-    String peek(int length);
-
-    /**
-     * Remembers the current position in the document, returning the corresponding snapshot.
-     * @return the snapshot representing the current position of the document reader
-     */
-    ReaderSnapshot remember();
-
-    /**
-     * Forgets the latest remembered position
-     * @return the state being forgotten
-     */
-    ReaderSnapshot forget();
-
-    /**
-     * Rewinds back to the latest remembered position
-     */
-    void rewind();
-
-    /**
-     * Repositions the reader according to the snapshot
-     * @param snapshot    the snapshot to reposition to
-     */
-    void reposition(ReaderSnapshot snapshot);
 
     /**
      * @return the current line of the document at which the reader stands. Starts at 1.
