d8a89adde6fe66458f7f3149b7f0070bb6aa07a9
Milad Naseri <milad.naseri@cdk.com>
2016-04-11 12:08:45 -0700

    feat(~.tools.PropertyUtils): add support for a more comprehensive property get/set
    We can now safely read nested properties when something in the middle is `null`, or attempt to set values without having to rely on Spring's way of handling bean wrapping

--- src/main/java/com/mmnaseri/utils/spring/data/tools/PropertyUtils.java
+++ src/main/java/com/mmnaseri/utils/spring/data/tools/PropertyUtils.java
@@ -4,12 +4,11 @@ import com.mmnaseri.utils.spring.data.query.PropertyDescriptor;
 import com.mmnaseri.utils.spring.data.query.impl.ImmutablePropertyDescriptor;
 import com.mmnaseri.utils.spring.data.string.DocumentReader;
 import com.mmnaseri.utils.spring.data.string.impl.DefaultDocumentReader;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,7 +20,7 @@ import java.util.Map;
  */
 public final class PropertyUtils {
 
-    private static final Map<Class<?>, Class<?>> types = new HashMap<Class<?>, Class<?>>();
+    private static final Map<Class<?>, Class<?>> types = new HashMap<>();
 
     private PropertyUtils() {
         throw new UnsupportedOperationException();
@@ -39,17 +38,93 @@ public final class PropertyUtils {
     }
 
     public static Object getPropertyValue(Object context, String property) {
-        if (property.contains(".")) {
-            final String[] split = property.split("\\.", 2);
-            final BeanWrapper wrapper = new BeanWrapperImpl(context);
-            final Object propertyValue = wrapper.getPropertyValue(split[0]);
-            if (propertyValue == null) {
+        final String[] chain = property.split("\\.");
+        final List<Object> accessors = new ArrayList<>();
+        Class<?> type = context.getClass();
+        final StringBuilder path = new StringBuilder();
+        for (String current : chain) {
+            if (path.length() > 0) {
+                path.append('.');
+            }
+            path.append(current);
+            final Method getter = ReflectionUtils.findMethod(type, "get" + StringUtils.capitalize(current));
+            if (getter != null) {
+                accessors.add(getter);
+                type = getter.getReturnType();
+            } else {
+                final Field field = ReflectionUtils.findField(type, current);
+                if (field != null) {
+                    field.setAccessible(true);
+                    accessors.add(field);
+                    type = field.getType();
+                } else {
+                    throw new IllegalStateException("Cannot find property `" + path + "` on type `" + context.getClass() + "`");
+                }
+            }
+        }
+        Object result = context;
+        for (Object accessor : accessors) {
+            try {
+                if (accessor instanceof Method) {
+                    final Method method = (Method) accessor;
+                    result = method.invoke(result);
+                } else {
+                    final Field field = (Field) accessor;
+                    result = field.get(result);
+                }
+            } catch (Exception e) {
+                throw new IllegalStateException("Failed to access property `" + property + "`", e);
+            }
+            if (result == null) {
                 return null;
             }
-            return getPropertyValue(propertyValue, split[1]);
         }
-        final BeanWrapper wrapper = new BeanWrapperImpl(context);
-        return wrapper.getPropertyValue(property);
+        return result;
+    }
+
+    public static Object setPropertyValue(Object context, String property, Object value) {
+        if (property.contains(".")) {
+            final String[] split = property.split("\\.", 2);
+            final Object propertyValue = getPropertyValue(context, split[0]);
+            if (propertyValue == null) {
+                throw new IllegalStateException("Root property " + split[0] + " was null when reading " + property);
+            }
+            return setPropertyValue(propertyValue, split[1], value);
+        }
+        Method setter = null;
+        final String setterName = "set" + StringUtils.capitalize(property);
+        if (value != null) {
+            setter = ReflectionUtils.findMethod(context.getClass(), setterName, value.getClass());
+        } else {
+            for (Method method : ReflectionUtils.getAllDeclaredMethods(context.getClass())) {
+                if (method.getName().equals(setterName) && method.getParameterTypes().length == 1 && !method.getParameterTypes()[0].isPrimitive()) {
+                    setter = method;
+                    break;
+                }
+            }
+        }
+        if (setter != null) {
+            try {
+                setter.invoke(context, value);
+                return context;
+            } catch (Exception e) {
+                throw new IllegalStateException("Failed to set property value through the setter method " + setter);
+            }
+        } else {
+            final Field field = ReflectionUtils.findField(context.getClass(), property);
+            if (field != null) {
+                if (!Modifier.isFinal(field.getModifiers())) {
+                    field.setAccessible(true);
+                }
+                try {
+                    field.set(context, value);
+                    return context;
+                } catch (Exception e) {
+                    throw new IllegalStateException("Failed to set property value through the field " + field);
+                }
+            }
+        }
+        throw new IllegalStateException("Failed to find property " + property + " on " + context.getClass());
     }
 
     public static PropertyDescriptor getPropertyDescriptor(Class<?> domainType, String expression) {
@@ -63,7 +138,7 @@ public final class PropertyUtils {
         }
         Class<?> context = domainType;
         final DocumentReader reader = new DefaultDocumentReader(expression);
-        final List<String> tokens = new ArrayList<String>();
+        final List<String> tokens = new ArrayList<>();
         while (reader.hasMore()) {
             tokens.add(reader.expect("[A-Z][a-z]*"));
         }
