66f30298bac455d589ac76160462b9a6ddd7c952
Milad Naseri <mmnaseri@programmer.net>
2016-04-28 21:43:53 -0700

    feat(all): add support for QueryDslPredicateExecutor
    This goes toward #3

--- spring-data-mock/src/main/java/com/mmnaseri/utils/spring/data/proxy/impl/DefaultTypeMappingContext.java
+++ spring-data-mock/src/main/java/com/mmnaseri/utils/spring/data/proxy/impl/DefaultTypeMappingContext.java
@@ -3,10 +3,7 @@ package com.mmnaseri.utils.spring.data.proxy.impl;
 import com.mmnaseri.utils.spring.data.error.RepositoryDefinitionException;
 import com.mmnaseri.utils.spring.data.proxy.TypeMapping;
 import com.mmnaseri.utils.spring.data.proxy.TypeMappingContext;
-import com.mmnaseri.utils.spring.data.repository.DefaultCrudRepository;
-import com.mmnaseri.utils.spring.data.repository.DefaultGemfireRepository;
-import com.mmnaseri.utils.spring.data.repository.DefaultJpaRepository;
-import com.mmnaseri.utils.spring.data.repository.DefaultPagingAndSortingRepository;
+import com.mmnaseri.utils.spring.data.repository.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.core.annotation.AnnotationAwareOrderComparator;
@@ -55,6 +52,10 @@ public class DefaultTypeMappingContext implements TypeMappingContext {
             if (ClassUtils.isPresent("org.springframework.data.jpa.repository.JpaRepository", ClassUtils.getDefaultClassLoader())) {
                 log.debug("JPA support is enabled in this project, so we need to support the methods");
                 register(Object.class, DefaultJpaRepository.class);
+            }
+            if (ClassUtils.isPresent("org.springframework.data.querydsl.QueryDslPredicateExecutor", ClassUtils.getDefaultClassLoader())) {
+                log.debug("QueryDSL support is enabled. We will add the proper method implementations.");
+                register(Object.class, DefaultQueryDslPredicateExecutor.class);
             }
             register(Object.class, DefaultPagingAndSortingRepository.class);
             register(Object.class, DefaultCrudRepository.class);
--- spring-data-mock/src/main/java/com/mmnaseri/utils/spring/data/repository/DefaultPagingAndSortingRepository.java
+++ spring-data-mock/src/main/java/com/mmnaseri/utils/spring/data/repository/DefaultPagingAndSortingRepository.java
@@ -1,21 +1,14 @@
 package com.mmnaseri.utils.spring.data.repository;
 
 import com.mmnaseri.utils.spring.data.domain.DataStoreAware;
-import com.mmnaseri.utils.spring.data.domain.impl.PropertyComparator;
-import com.mmnaseri.utils.spring.data.query.NullHandling;
-import com.mmnaseri.utils.spring.data.query.Order;
-import com.mmnaseri.utils.spring.data.query.SortDirection;
-import com.mmnaseri.utils.spring.data.query.impl.ImmutableOrder;
-import com.mmnaseri.utils.spring.data.query.impl.ImmutableSort;
 import com.mmnaseri.utils.spring.data.store.DataStore;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
-import java.util.LinkedList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -23,7 +16,7 @@ import java.util.List;
  * @since 1.0 (10/12/15)
  */
 @SuppressWarnings("WeakerAccess")
-public class DefaultPagingAndSortingRepository implements DataStoreAware {
+public class DefaultPagingAndSortingRepository extends PagingAndSortingSupport implements DataStoreAware {
 
     private static final Log log = LogFactory.getLog(DefaultPagingAndSortingRepository.class);
 
@@ -35,34 +28,7 @@ public class DefaultPagingAndSortingRepository implements DataStoreAware {
      * @return sorted entries, unless sort is null.
      */
     public List findAll(Sort sort) {
-        log.info("Loading all the data in the data store");
-        //noinspection unchecked
-        final List list = new LinkedList(dataStore.retrieveAll());
-        if (sort == null) {
-            log.info("No sort was specified, so we are just going to return the data as-is");
-            return list;
-        }
-        final List<Order> orders = new LinkedList<>();
-        for (Sort.Order order : sort) {
-            final SortDirection direction = order.getDirection().equals(Sort.Direction.ASC) ? SortDirection.ASCENDING : SortDirection.DESCENDING;
-            final NullHandling nullHandling;
-            switch (order.getNullHandling()) {
-                case NULLS_FIRST:
-                    nullHandling = NullHandling.NULLS_FIRST;
-                    break;
-                case NULLS_LAST:
-                    nullHandling = NullHandling.NULLS_LAST;
-                    break;
-                default:
-                    nullHandling = NullHandling.DEFAULT;
-                    break;
-            }
-            final Order derivedOrder = new ImmutableOrder(direction, order.getProperty(), nullHandling);
-            orders.add(derivedOrder);
-        }
-        log.info("Sorting the retrieved data: " + orders);
-        PropertyComparator.sort(list, new ImmutableSort(orders));
-        return list;
+        return sort(retrieveAll(), sort);
     }
 
     /**
@@ -71,28 +37,17 @@ public class DefaultPagingAndSortingRepository implements DataStoreAware {
      * @return the specified view of the data
      */
     public Page findAll(Pageable pageable) {
-        final List<?> all;
-        if (pageable.getSort() != null) {
-            log.info("The page specification requests sorting, so we are going to sort the data first");
-            all = findAll(pageable.getSort());
-        } else {
-            log.info("The page specification does not need sorting, so we are going to load the data as-is");
-            //noinspection unchecked
-            all = new LinkedList(dataStore.retrieveAll());
-        }
-        int start = Math.max(0, pageable.getPageNumber() * pageable.getPageSize());
-        int end = start + pageable.getPageSize();
-        start = Math.min(start, all.size());
-        end = Math.min(end, all.size());
-        log.info("Trimming the selection down for page " + pageable.getPageNumber() + " to include items from " + start + " to " + end);
-        final List<?> selection = new LinkedList<>(all.subList(start, end));
-        //noinspection unchecked
-        return new PageImpl(selection, pageable, all.size());
+        return page(retrieveAll(), pageable);
     }
 
     @Override
     public void setDataStore(DataStore dataStore) {
         this.dataStore = dataStore;
     }
-    
+
+    private Collection retrieveAll() {
+        log.info("Loading all the data in the data store");
+        return dataStore.retrieveAll();
+    }
+
 }
