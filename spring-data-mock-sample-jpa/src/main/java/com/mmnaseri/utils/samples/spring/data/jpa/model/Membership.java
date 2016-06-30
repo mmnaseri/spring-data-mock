9e38cd3ceb874cfae01fbe08d48ea735f251246a
Naseri, Milad <milad.naseri@cdk.com>
2016-06-30 09:42:20 -0700

    feat(sample-jpa): add a user-group sample to demonstrate how many-to-many relations (and relations in general) are handled
    closes #127

---
+++ spring-data-mock-sample-jpa/src/main/java/com/mmnaseri/utils/samples/spring/data/jpa/model/Membership.java
@@ -0,0 +1,45 @@
+package com.mmnaseri.utils.samples.spring.data.jpa.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ * @author Milad Naseri (milad.naseri@cdk.com)
+ * @since 1.0 (6/29/16, 4:04 PM)
+ */
+@Entity
+public class Membership {
+
+    @Id
+    private String id;
+    @ManyToOne
+    private User user;
+    @ManyToOne
+    private Group group;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+}
