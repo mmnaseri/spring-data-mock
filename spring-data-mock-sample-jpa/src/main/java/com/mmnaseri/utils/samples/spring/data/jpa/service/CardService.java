9f5e6659d978a1b7340bde70e8111b590e971e84
Milad Naseri <m.m.naseri@gmail.com>
2020-10-29 13:30:27 -0700

    fix(query): Fix ordering when no query parameter is specific
    This fixes #175 by allowing expressions such as `findAllByOrderBy...`

---
+++ spring-data-mock-sample-jpa/src/main/java/com/mmnaseri/utils/samples/spring/data/jpa/service/CardService.java
@@ -0,0 +1,11 @@
+package com.mmnaseri.utils.samples.spring.data.jpa.service;
+
+import com.mmnaseri.utils.samples.spring.data.jpa.model.Card;
+
+import java.util.List;
+
+public interface CardService {
+
+  List<Card> load();
+
+}
